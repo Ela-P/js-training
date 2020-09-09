4dc7925c1b11e53408c556241e550a2b5903b40d
Elena P <elaina.prokudova@gmail.com>
2020-09-09 14:04:33 +0300

    Add user

--- 00/main.js
+++ 00/main.js
@@ -3,3 +3,18 @@ const surname = 'Grid';
 let age = 26;
 
 alert(`Hello, world! Встречай, JS! Я ${name} ${surname}, и мне ${age}. Рад встрече!`)
+
+const user = {
+    name: 'Misha',
+    height: 175,
+    age: 33,
+    badhabits: 'no',
+    namefriends: `'Alex', 'Roma', 'Ihar', 'Ivan', 'Oleg'`,
+};
+console.log(user.height);
+
+//user.canSingSongs = 'yes';
+
+//delete user.namefriends;
+
+//console.log(user);

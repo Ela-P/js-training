f7cdd8b93ec50ae91c7766543f1a3da71fb4b92e
Elena P <elaina.prokudova@gmail.com>
2020-09-14 14:03:28 +0300

    Add 3 task

--- 01/index.js
+++ 01/index.js
@@ -10,13 +10,50 @@
 //}
 //fizzBuzz(23);
 
-function toNumber(value) {
-    const num = "1235";
-    const number = +num;
-    console.log(value);
+//function toNumber(value) {
+    //const num = "1235";
+    //const number = +num;
+    //console.log(value);
     
-        return num != "1235" ? null : 0
+  //      return num != "1235" ? null : 0
     
-}
+//}
 
-toNumber(12548468546854);
+//toNumber(12548468546854);
+
+
+//function createUser(name, age, height, weight) {- не знаю как правильно сделать
+//    let person = { 
+     //   name: string, 
+     //   weight: number, 
+     //   age: number, 
+     //   height: number, 
+     //   skills: {
+
+     //   }
+
+
+
+
+
+    //const string = +name;
+    //const num = Number(age);
+   // const nume = Number(height);
+   // const numb = Number(weight);
+   // console.log(typeof name);
+   // console.log(typeof age);
+   // console.log(typeof height);
+   // console.log(typeof weight);
+    
+
+    
+ //   if (numb < 100) {
+  //      return true;
+   // }
+//}
+
+
+
+//createUser("кот-обормот", 22, 150, 60);
+
+  

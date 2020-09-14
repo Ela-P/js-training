574a9a4844de1002cabad20b1e76f3014224532e
Elena P <elaina.prokudova@gmail.com>
2020-09-14 11:21:32 +0300

    Add changed % number

--- 01/index.js
+++ 01/index.js
@@ -1,13 +1,22 @@
 function fizzBuzz(n) {
     for (let i = 0; i <= n; i++) {
-        if (!(i % 3 === 0))
+    if (!(i % 3 === 0))
         console.log("fizz");
-        else if (!(i % 5 === 0))
+    else if (!(i % 5 === 0))
         console.log ("Buzz");
-        else (!(i % 3 & 5 === 0))
+    else (!(i % 3 & 5 === 0))
         console.log ("fizzBuzz!");
-        
     }
 }
-   
-fizzBuzz (23);
+fizzBuzz(23);
+
+    //function toNumber(value) {
+    // const num = "1235";
+    //    value = Number(num);
+     //   console.log(value);
+    //    if (num != "1235") {
+    //     console.log(null);
+   // }
+//}
+
+//toNumber(jjj);

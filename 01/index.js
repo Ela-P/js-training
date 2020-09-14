c5fdb4a0da1ba894ad33b45e71d3428ecc8a6c71
Elena P <elaina.prokudova@gmail.com>
2020-09-14 11:44:01 +0300

    Add transformation string

--- 01/index.js
+++ 01/index.js
@@ -1,22 +1,22 @@
-function fizzBuzz(n) {
-    for (let i = 0; i <= n; i++) {
-    if (!(i % 3 === 0))
-        console.log("fizz");
-    else if (!(i % 5 === 0))
-        console.log ("Buzz");
-    else (!(i % 3 & 5 === 0))
-        console.log ("fizzBuzz!");
-    }
-}
-fizzBuzz(23);
-
-    //function toNumber(value) {
-    // const num = "1235";
-    //    value = Number(num);
-     //   console.log(value);
-    //    if (num != "1235") {
-    //     console.log(null);
-   // }
+//function fizzBuzz(n) {
+// for (let i = 0; i <= n; i++) {
+// if (!(i % 3 === 0))
+//     console.log("fizz");
+// else if (!(i % 5 === 0))
+//     console.log ("Buzz");
+// else (!(i % 3 & 5 === 0))
+//     console.log ("fizzBuzz!");
+// }
 //}
+//fizzBuzz(23);
 
-//toNumber(jjj);
+function toNumber(value) {
+    const num = "1235";
+    const number = +num;
+    console.log(value);
+    
+        return num != "1235" ? null : 0
+    
+}
+
+toNumber(12548468546854);

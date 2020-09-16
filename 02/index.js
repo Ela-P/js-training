912d37715e5922237e39acc63a7df7cc6ee07f51
Elena P <elaina.prokudova@gmail.com>
2020-09-16 14:22:27 +0300

    Add sayHallo 0 task

---
+++ 02/index.js
@@ -0,0 +1,17 @@
+// Задание 0
+//sayHello('Victor');
+//sayHello('Mark');
+
+
+function sayHello(name) {
+    if (name === "Mark") {
+        alert(`Hi, Mark`)
+    }
+    else {
+        alert(`Hello, ${name}`)
+    }
+}
+
+
+// Задание 1 
+

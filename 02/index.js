5c4c792a038dd2c80eeaf4cb9acb89a39b90a5bf
Elena P <elaina.prokudova@gmail.com>
2020-09-16 16:03:14 +0300

    Add iisEven 3 task

--- 02/index.js
+++ 02/index.js
@@ -14,10 +14,36 @@ function sayHello(name) {
 
 
 // Задание 1 
+//console.log(min(2, 5));
+//console.log(min(3, -1));
+//console.log(min(1, 1));
 
 function min(a, b) {
     return a < b ? a : b;
 }
-//console.log(min(2, 5));
-//console.log(min(3, -1));
-//console.log(min(1, 1));
+
+
+//Задание 2
+
+//function pow(x, n) {
+// let x = 
+// let n = 
+ // if (n < 1) {
+  //alert(`n- только натуральное число`);
+//} else {
+ // alert(pow(x, n));
+
+//}
+//}
+  
+//pow(3, 3);
+
+
+//Задание 3
+//console.log(isEven(2));
+//console.log(isEven(5));
+
+function isEven(n) {
+    return n % 2 ? false : true;
+}
+

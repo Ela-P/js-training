9eb440f78d689eaf3d24349dfad4a3a2be619987
Elena P <elaina.prokudova@gmail.com>
2020-09-16 17:43:41 +0300

    Add floor 5 task

--- 02/index.js
+++ 02/index.js
@@ -47,3 +47,32 @@ function isEven(n) {
     return n % 2 ? false : true;
 }
 
+
+
+//Задание 4 - не разобралась 
+//function deleteChars(str) 
+
+
+
+//Задание 5
+    
+function convertFloor(floor) {
+    for (let i = -14; i <= floor; i++) {
+        if (i === 1 && i === -1) {
+            console.log(0)
+        } else if (i === 13 && i === -13) {
+            console.log(12)
+        }
+    }
+}
+//console.log(convertFloor(-1)); // -1
+
+//console.log(convertFloor(2)); // 3
+
+//console.log(convertFloor(2)); // 3
+
+//console.log(convertFloor(0)); // 1
+
+//console.log(convertFloor(12)); // 13
+
+//console.log(convertFloor(14)); // 14

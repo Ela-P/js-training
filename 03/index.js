3246e09474871c54c827b7aed90ac22042027e6f
Elena P <elaina.prokudova@gmail.com>
2020-09-19 12:36:43 +0300

    Add user task

--- 03/index.js
+++ 03/index.js
@@ -2,9 +2,23 @@
 
 const car = {
     name: "Audi",
-    doors: 3
+    doors: 3,
 };
 for (let key in car) {
-    alert(key);
-    alert(car[key])
+   // alert(key);
+  //  alert(car[key])
 }
+
+
+// Задание 2 
+
+const user = {
+    name: "Anton",
+    email: "pitbool@mail.ru",
+    phone: 375291232623,
+    //[id]: 1111,
+};
+
+const newUser = Object.assign({}, user);
+
+//console.log(newUser);

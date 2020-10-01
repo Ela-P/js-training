d6d7c0e41014ccfa6aba5a19c0d51ac391eff616
Elena P <elaina.prokudova@gmail.com>
2020-10-01 12:45:15 +0300

    Add valid class form, click btn-> valid form

--- 06/index.js
+++ 06/index.js
@@ -5,36 +5,70 @@ const btn = document.getElementById("btn");
 
 //console.log(form);
 
+//класс присвоения
 
-// объект 
+const INVALID_CLASS = "invalid";
+
+
+// объект c валидацией
 const formHelper = {
     login: {
         value: "",
         valid: false,
+        checkValidation() {
+            this.login.valid = this.login.value !== "";
+        }
     },
     name: {
         value: "",
         valid: false,
+        checkValidation() {
+            this.name.valid = this.name.value !== "";
+        },
     },
     age: {
         value: "",
         valid: false,
+        checkValidation() {
+            this.age.valid = this.age.value !== "";
+        },
     },
     email: {
         value: "",
         valid: false,
+        checkValidation() {
+            this.email.valid = this.email.value !== "";
+        },
     },
     city: {
         value: "",
         valid: false,
+        checkValidation() {
+            this.city.valid = this.city.value !== "";
+        },
     },
     password: {
         value: "",
         valid: false,
+        checkValidation() {
+            this.password.valid = this.password.value !== "";
+        },
     },
     repeatPassword: {
         value: "",
         valid: false,
+        checkValidation() {
+            this.repeatPassword.valid = 
+            this.password.value === this.repeatPassword.value;
+        },
+    },
+    checkFormValid() { 
+        for (const key in this){
+            if (typeof this[key] !== "function" && this[key].valid === false) {
+                return false;
+            }
+        } 
+        return true;
     },
 };
 
@@ -44,15 +78,33 @@ form.addEventListener("input", (event) => {
     const value = event.target.value;
     const name = event.target.name;
   
-    formHelper[name] = value;
-})
+    formHelper[name].value = value;
+    const bindValid = formHelper[name].checkValidation.bind(formHelper);
+    bindValid(); // валидность формы
 
-//на кнопку
+    btn.disabled = !formHelper.checkFormValid(); //(не) приводит кнопку к активности
+
+    classInvalidAdd (event.target, formHelper[name].valid); //(не) добавление класса валидности
+});
+
+
+// Присвоения класса валидации
+
+function classInvalidAdd(domNode, isValid) {
+    if (isValid) {
+        domNode.classList.remove(INVALID_CLASS)
+    } else {
+        domNode.classList.add(INVALID_CLASS)
+    }
+}
+
+//Деф на кнопку
+
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
 
  //console.log(formHelper);
-})
+});
 
 

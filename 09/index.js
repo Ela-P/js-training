db9d02b1a090f7c3eb4c3b0adbfbba15c3012a01
Elena P <elaina.prokudova@gmail.com>
2020-10-10 19:57:11 +0300

    Corrected view columns table

--- 09/index.js
+++ 09/index.js
@@ -3,7 +3,7 @@ const arrUsers = [
     company: "Google", 
     email: "alex@gmail.com", 
     phone: +375292929292, 
-    balance: "$3,323", 
+    balance: "$130,323", 
     isActive: false, 
     gender: "female"}, 
 
@@ -29,19 +29,37 @@ const arrUsers = [
     phone: +375292986965, 
     balance: "$15,236",
     isActive: false,
-    gender: "female"}
+    gender: "female"},
+
+    {name: "Kolya",
+    company: "Odopod", 
+    email: "K.ol@gmail.com",
+    phone: +375292523696, 
+    balance: "$19,237",
+    isActive: false,
+    gender: "female"},
+
+    {name: "Vika", 
+    company: "Blammo", 
+    email: "V.blam@gmail.com", 
+    phone: +375291236361, 
+    balance: "$15,346", 
+    isActive: false, 
+    gender: "Mele"}, 
 ];
 
 
 function makeTable(arrUsers) {
     const table = document.createElement('table');
-    for (let i = 0; i < arrUsers.lengt; i++) {
+    for (let i = 0; i < arrUsers.length; i++) {
             const row = document.createElement("tr");
             const currentUser = arrUsers[i];
 
-        for (let key in currentUser) {
-            const cell = document.createElement("td");
-            cell.innerHTML = currentUser[key];
+            const { name, company, email, phone, balance } = arrUsers[i]; 
+
+            for (const value of [name, company, email, phone, balance]) {
+              const cell = document.createElement("td");
+              cell.innerHTML = value; 
 
             row.appendChild(cell);
         }
@@ -51,5 +69,7 @@ function makeTable(arrUsers) {
     return document.body.appendChild(table);
 }
 
-makeTable([arrUsers]);
+makeTable(arrUsers);
 
+//console.log([arrUsers]);
+//console.log(arrUsers);
